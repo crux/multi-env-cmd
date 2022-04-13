0fb602098a7e4e8ce0eb8fd0ae4f0b8eb3c99e05
dirk lüsebrink <dirk.luesebrink@gmail.com>
2022-04-13 11:46:31 +0200

    added usage info and argument checking

--- multi-env.js
+++ multi-env.js
@@ -5,11 +5,19 @@ const yargs = require("yargs");
 const {spawn} = require('child_process')
 const { hideBin } = require('yargs/helpers')
 
+function usage_and_exit() {
+    console.log('usage: multi-env <env file> ... -- <shell command>')
+    process.exit(1)
+}
+
 const argv = process.argv
-//console.log(process.argv);
 const envs = argv.slice(0, argv.indexOf('--')).slice(2)
 const cmd = argv.slice(argv.indexOf('--') + 1).join(' ')
-//console.log({envs: envs, cmd: cmd})
+
+// bail out on empty env file list
+;(0 < envs.length) || usage_and_exit()
+// bail out on empty cmdline
+;(cmd.length < 1) && usage_and_exit()
 
 var line = cmd
 if(0 < envs.length) {
